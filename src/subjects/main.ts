185b6ee595a112448f802764b72509d0bcc0fde3
Ateeb Sohail <challengebudies9@gmail.com>
2024-08-12 16:48:29 +0500

    Physics Class addition

--- src/subjects/main.ts
+++ src/subjects/main.ts
@@ -2,6 +2,7 @@ import { SubjectBuilder } from "src/modules/classes"
 
 const questions = [
     new SubjectBuilder("0620", 'Chemistry', 'IGCSE', ['Some topic']),
+    new SubjectBuilder("0625", 'Physics', 'IGCSE', ['Some topic']),
 ]
 
 export default questions
